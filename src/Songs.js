5ed7841ac83aa4dcef8092277617caf8a14e13ce
Sky Lin <linsky519@gmail.com>
2021-12-06 16:49:37 -0700

    created song file

--- src/Songs.js
+++ src/Songs.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { Button, Table } from "semantic-ui-react";
-import { Popup, Card, Image, Rating } from 'semantic-ui-react';
+import { Button, Card, Image } from 'semantic-ui-react';
 
 const Songs = ({ songs }) => {
   const renderSongs = () => {
@@ -33,59 +32,3 @@ const Songs = ({ songs }) => {
   )
 }
 export default Songs;
-
-
-
-
-// const Songs = ({ songs }) => (
-//   <Table color="green" inverted>
-//     <Table.Header>
-//       <Table.Row>
-//         <Table.HeaderCell>Song Title</Table.HeaderCell>
-//         <Table.HeaderCell>Artist</Table.HeaderCell>
-//         <Table.HeaderCell>Options</Table.HeaderCell>
-//       </Table.Row>
-//     </Table.Header>
-
-//     <Table.Body>
-//       {
-//         songs.map(song => (
-//           <Table.Row key = {song.id}>
-//             <Table.Cell>{song.name}</Table.Cell>
-//             <Table.Cell>{song.artist}</Table.Cell>
-//             <Table.Cell>
-//               <Button color = "red">
-//                 Read
-//               </Button>
-//               <Button>
-//                 Delete
-//               </Button>
-//             </Table.Cell>
-//           </Table.Row>
-//         ))
-//       }
-//     </Table.Body>
-//   </Table>
-// );
-
-// export default Songs;
-
-
-  // <Popup
-  //   trigger={
-  //     <Card>
-  //       <Image src='https://m.media-amazon.com/images/I/81U6o32LuUL._SL1500_.jpg' />
-  //       <Card.Content>
-  //         <Card.Header>All I Want For Christmas Is You</Card.Header>
-  //         <Card.Description>
-  //           Mariah Carey
-  //         </Card.Description>
-  //       </Card.Content>
-  //     </Card>
-  //   }
-  // >
-  //   <Popup.Header>Rating</Popup.Header>
-  //   <Popup.Content>
-  //     <Rating icon='star' defaultRating={5} maxRating={5} />
-  //   </Popup.Content>
-  // </Popup>
