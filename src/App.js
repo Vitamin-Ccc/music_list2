82c4cc0acb4d9a5a091fab224dec7d1d5eeb1ce4
Sky Lin <linsky519@gmail.com>
2021-12-06 16:48:23 -0700

    images added

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import Songs from './Songs';
 class App extends Component{
   state = {
     songs: [
-      {id:1, name: "Run", artist: "BTS", },
-      {id:2, name: "All I Want for Christmas Is You", artist: "Mariah Carey", image: "https://m.media-amazon.com/images/I/81U6o32LuUL._SL1500_.jpg"},
-      {id:3, name: "It's Beginning to Look a Lot Like Christmas", artist: "Michael Buble",},
-      {id:4, name: "Santa Tell Me", artist: "Ariana Grande",},
+      {id:1, name: "All I Want for Christmas Is You", artist: "Mariah Carey", image: "https://m.media-amazon.com/images/I/81U6o32LuUL._SL1500_.jpg"},
+      {id:2, name: "It's Beginning to Look a Lot Like Christmas", artist: "Michael Buble", image: "https://m.media-amazon.com/images/I/61M3mNpxJML._SX355_.jpg"},
+      {id:3, name: "Santa Tell Me", artist: "Ariana Grande", image: "https://m.media-amazon.com/images/I/61HbB2VISCL._SS500_.jpg"},
+      {id:4, name: "Jingle Bell Rock", artist: "Bobby Helms", image: "https://thinkcountrymusic.com/wp-content/uploads/2020/12/bobby-helms-jingle-bell-rock.jpg" },
     ]
   }
 
